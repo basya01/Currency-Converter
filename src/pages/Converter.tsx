afdd854a03f2888b57704308306dbba8d5702e4a
basya01 <arsen.lulukyan@gmail.com>
2022-09-30 19:03:38 +0300

    add default value input

--- src/pages/Converter.tsx
+++ src/pages/Converter.tsx
@@ -1,7 +1,8 @@
-import { KeyboardEvent, useState } from 'react';
+import { KeyboardEvent, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import { Input } from '../components/Input';
 import { useAppSelector } from '../hooks/redux';
+import { Status } from '../store/slices/rates';
 import { calcRate } from '../utils/calcRate';
 
 const InputStyle = styled(Input)`
@@ -11,8 +12,12 @@ const InputStyle = styled(Input)`
   border-radius: 30px;
 `;
 
-const ResultLabel = styled.p`
+const ResultsBlock = styled.div`
+  text-align: center;
   margin-top: 50px;
+`;
+
+const ResultLabel = styled.p`
   font-size: 30px;
 `;
 
@@ -22,28 +27,42 @@ const Result = styled.p`
 `;
 
 const Converter = () => {
-  const [result, setResult] = useState<{ cur: string; value: number } | null>();
-  const { rates } = useAppSelector((state) => state.rates.data);
+  const [result, setResult] = useState<{ cur: string; value: number } | null>(null);
+  const [value, setValue] = useState('"15 usd" in "uah"');
+  const { data, status } = useAppSelector((state) => state.rates);
 
   const inputKeyHandler = (e: KeyboardEvent<HTMLInputElement>) => {
     if (!(e.code === 'Enter')) return;
 
-    setResult(calcRate((e.target as HTMLInputElement).value, rates));
+    setResult(calcRate(value, data.rates));
   };
+
+  useEffect(() => {
+    setResult(calcRate(value, data.rates));
+  }, [data.rates]);
 
   return (
     <>
-      <InputStyle onKeyDown={inputKeyHandler} placeholder='Example: "15 usd" in "uah"' />
-      {result !== undefined && (
-        <>
-          <ResultLabel>Result: </ResultLabel>
-          <Result>
-            {result
-              ? `${Math.round(result.value * 1000) / 1000} ${result.cur.toUpperCase()}`
-              : 'Error'}
-          </Result>
-        </>
-      )}
+      <InputStyle
+        onKeyDown={inputKeyHandler}
+        onChange={(e) => setValue(e.target.value)}
+        value={value}
+        placeholder='Example: "15 usd" in "uah"'
+      />
+      <ResultsBlock>
+        {status === Status.PENDING ? (
+          <p>Загрузка...</p>
+        ) : (
+          <>
+            <ResultLabel>Result: </ResultLabel>
+            <Result>
+              {result
+                ? `${Math.round(result.value * 1000) / 1000} ${result.cur.toUpperCase()}`
+                : 'Error'}
+            </Result>
+          </>
+        )}
+      </ResultsBlock>
     </>
   );
 };
